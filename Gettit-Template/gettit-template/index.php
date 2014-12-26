da9a9a485cc15b14179877b76c5adc685b292235
G1nux <unconfigured@null.spigotmc.org>
2014-12-26 13:10:38 +0100

    Set up Workspace directory

---
+++ Gettit-Template/gettit-template/index.php
@@ -0,0 +1,10 @@
+<?php
+<head>
+  <jdoc:include type="head" />
+</head>
+<body>
+  <jdoc:include type="modules" name="meine-topposition" />
+  <jdoc:include type="message" />
+  <jdoc:include type="component" />
+</body>
+?>
